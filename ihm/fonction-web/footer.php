bfe73638b37bb5d5150fbcbe17fc3e7b7830dd85
pxcleme <55398201+pxcleme@users.noreply.github.com>
2021-04-27 16:35:47 +0200

    modif
    modif

--- ihm/fonction-web/footer.php
+++ ihm/fonction-web/footer.php
@@ -2,7 +2,8 @@
 <footer>
             
     <div>
-        <p class="copyright">Copyright :  2021-2022 - Tous droits réservés</p>
+        <p class="copyright">Copyright : site web la providence  2021-2022 - Tous droits réservés</p>
+        <p>realiser par le BTS SN1 et SN2</p>
     </div>
 
 </footer> 
