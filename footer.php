9dcedaa8e220254ce812aa24a38faf9245f65cc9
pxcleme <55398201+pxcleme@users.noreply.github.com>
2021-04-27 15:35:27 +0200

    petite modif
    petite modif

--- footer.php
+++ footer.php
@@ -1,22 +1,12 @@
-<?php
 
-
-function footer()
-{
-
-    ?>
-
-
-    <footer>
-    
-    
-    
-    </footer>
-
-
-
- <?php
-
-}
-
-?>
+<!-- Pied de page du site -->
+<footer> 
+    <div>
+        <h3>Me contacter</h3>
+        
+        <div>
+            <p>Copyright : 2021-2022 - Tous droits réservés</p>
+        </div>
+                
+    </div>
+</footer> 
