63d1050af67b021a10526a3f30fba846d4eaf878
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-04-28 16:23:36 +0200

    Correction d'erreurs.

--- ihm/fonction-web/footer.php
+++ ihm/fonction-web/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div>
         <p class="copyright">Copyright : 2021 - <?= date("Y") ?> - Tous droits réservés.</p>
-        <p a href="http://www.la-providence.net/">realisé par les BTS SN1 et SN2 de la providence Amiens</p>
+        <p>Réalisé par les BTS SN1 et SN2 de <a href="http://www.la-providence.net/">La Providence - Amiens</a>.</p>
     </div>
 </footer>
