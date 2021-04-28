43bd537df3e615e1986965225397936d98c58093
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-04-28 16:19:44 +0200

    Optimisation Date.

--- ihm/fonction-web/footer.php
+++ ihm/fonction-web/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div>
-        <p class="copyright">Copyright : site web la providence  2021-2022 - Tous droits réservés</p>
+        <p class="copyright">Copyright : 2021 - <?= date("Y") ?> - Tous droits réservés.</p>
         <p a href="http://www.la-providence.net/">realisé par les BTS SN1 et SN2 de la providence Amiens</p>
     </div>
 </footer>
