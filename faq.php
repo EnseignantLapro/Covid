4c1c6c788c96ca34ee821f5aa9c8c8f29637c38c
ColsonB <bcolson@la-providence.net>
2021-04-29 11:31:40 +0200

    Ajout de titre et de questions

--- faq.php
+++ faq.php
@@ -4,6 +4,7 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
+        <title>FAQ</title>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,6 +14,9 @@
         <div class = "">
             <h1>FAQ</h1>
             <h3>Questions Fréquentes</h3>
+            <a href="">Comment se déplacer sur la carte ?</a><p></p>
+            <a href="">Comment utiliser un items ?</a><p></p>
+            <a href="">Comment créer un nouveau personnage ?</a>
         </div>
     </body>
 </html>
