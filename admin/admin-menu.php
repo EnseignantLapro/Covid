b3beff305b200bdd6d12d72507e538c7425ab4c9
nicolas-dealmeida <ndealmeida@la-providence.net>
2021-04-29 08:35:27 +0200

    COrrection d'erreur. Mise en place du Menu dans les Page Admin.

---
+++ admin/admin-menu.php
@@ -0,0 +1,25 @@
+<nav class="nav-Menu">
+    <ul class="ul-Menu">                
+        <li><a href="../index.php">Accueil</a></li>
+        <li><a href="../reglement.php">Règlement</a></li>
+        <li><a href="../combat.php">Combat</a></li>
+        <li><a href="../map.php">Map</a></li>
+        <li><a href="../classement.php">Classement</a></li>
+        <?php
+            if($Joueur1->isAdmin() == true){
+                ?>
+                    <li><a href="statistique.php">Statistique</a></li>
+                    <li><a href="index.php">Administration</a>
+                        <ul class="ul-SousMenu">
+                            <li><a href="admin-map.php">Gestion Map</a></li>
+                            <li><a href="admin-mods.php">Gestion Monstre</a></li>
+                            <li><a href="admin-map.php">Gestion Objet</a></li>
+                            <li><a href="admin-map.php">Gestion Personnage</a></li>
+                            <li><a href="admin-map.php">Gestion Utilisateur</a></li>
+                        </ul>
+                    </li>
+                <?php
+            }
+        ?>
+    </ul>
+</nav>
