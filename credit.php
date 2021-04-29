a76dcdab54a6216542a61f59056816e043605aff
pxcleme <55398201+pxcleme@users.noreply.github.com>
2021-04-29 12:12:03 +0200

    Add files via upload

---
+++ credit.php
@@ -0,0 +1,47 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>credit</title>
+    </head>
+
+    <body>
+
+    <center>
+        <h2>Les Acteur du Projet</h2>
+
+
+        <h1>Les BTS SN1</h1>
+        <p>Léa Bernard</p>
+        <p>Thomas Berthier</p>
+        <p>Louis Boucher</p>
+        <p>Alexandre Caré</p>
+        <p>Jeremy Caruelle</p>
+        <p>Clément Caudron</p>
+        <p>Clément Cauet</p>
+        <p>Baptiste Colson</p>
+        <p>Nicolas De almeida</p>
+        <p>Mathis Dechir</p>
+        <p>Gaëtan Deneufgermain</p>
+        <p>Maël Drelon</p>
+        <p>Kylian Duval</p>
+        <p>Gregory Febvin</p>
+        <p>Lucas Ghyselen</p>
+        <p>Julien Laridant</p>
+        <p>Romain Lienard</p>
+        <p>Julien Lussiez</p>
+        <p>Nicolas Marrocchi</p>
+        <p>Vincent Martel</p>
+        <p>Yann Martin</p>
+        <p>Valentin Pesant</p>
+        <p>Florian Rabasté</p>
+        <p>Evans Varnier</p>
+        <p>Maxence Vollet</p>
+
+
+    </center>
+
+        
+    </body>
+</html>
