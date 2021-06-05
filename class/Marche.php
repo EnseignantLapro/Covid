98333effc5269159d5e8cacb4670ef84ac361377
Pie Bleu <napiebleu@gmail.com>
2021-06-05 23:56:00 +0200

    Identations.

--- class/Marche.php
+++ class/Marche.php
@@ -2,34 +2,37 @@
     //cette classe est développé Cauet :  
     class Marche extends Map{
         /* PRIVATE */
-
         /* METHOD */
         public function __construct($bdd){
             parent::__construct($bdd);
         }            
-
         public function livraison($nbrItem){
             for($i=0; $i<$nbrItem; $i++){
                 $item = new item($this->_bdd);
                 $this->addItem($item->createItemAleatoire()); 
             }
         }
-
         public function acheter($entite, $idMap, $idEntite){
             $req = "SELECT mapitems.idMap, item.nom, item.valeur FROM `mapitems`, `item` WHERE item.id = mapitems.idItem AND `idMap` = $idMap";
             $RequetStatement = $this->_bdd->query($req);
-            ?><form method="post"><table><?php
-            while($Tab=$RequetStatement->fetch()){
-                ?>
-                    <tr>
-                        <td> <?= $Tab[1] ?> </td>
-                        <td> <?= $Tab[2] ?> </td>
-                        <td><input type="radio" name="radio[]" value="<?= $Tab[0] ?>"></td>
-                    </tr>
-                <?php
-            }
-            ?></table><input type="submit" name="acheter" value="Acheter"></form><?php
-
+            ?>
+                <form method="post">
+                    <table>
+                        <?php
+                            while($Tab=$RequetStatement->fetch()){
+                                ?>
+                                    <tr>
+                                        <td> <?= $Tab[1] ?> </td>
+                                        <td> <?= $Tab[2] ?> </td>
+                                        <td><input type="radio" name="radio[]" value="<?= $Tab[0] ?>"></td>
+                                    </tr>
+                                <?php
+                            }
+                        ?>
+                    </table>
+                    <input type="submit" name="acheter" value="Acheter">
+                </form>
+            <?php
             // Récupère l'argent du user
             $req = "SELECT user.money FROM `user`, `entite` WHERE user.idPersonnage=entite.id AND entite.id = $idEntite";
             $RequetStatement = $this->_bdd->query($req);
@@ -43,7 +46,9 @@
                     $valeur = $item->getValeur($checkId);
                 }
                 if($valeur > $money){
-                    echo "Vous n'avez pas assez d'argent";
+                    ?>
+                        <p>Vous n'avez pas assez d'argent</p>
+                    <?php
                 }else{
                     $entite->addItem($equipement);
                     $this->removeItemByID($checkId);
@@ -52,24 +57,28 @@
                     $RequetStatement = $this->_bdd->query($req);
                 }
             }
-
         }
-
         public function vendre($entite, $idEntite){
             $req = "SELECT persosacitems.idItem, item.nom, item.valeur FROM `persosacitems`, `item`, `user`, `entite` WHERE item.id = persosacitems.idItem AND user.idPersonnage = entite.id AND entite.id = $idEntite";
             $RequetStatement = $this->_bdd->query($req);
-            ?><form method="post"><table><?php
-            while($Tab=$RequetStatement->fetch()){
-                ?>
-                    <tr>
-                        <td> <?= $Tab[1] ?> </td>
-                        <td> <?= $Tab[2] ?> </td>
-                        <td><input type="checkbox" name="checkbox[]" value="<?= $Tab[0] ?>"></td>
-                    </tr>
-                <?php
-            }
-            ?></table><input type="submit" name="vendre" value="Vendre"></form><?php
-
+            ?>
+                <form method="post">
+                    <table>
+                        <?php
+                            while($Tab=$RequetStatement->fetch()){
+                                ?>
+                                    <tr>
+                                        <td> <?= $Tab[1] ?> </td>
+                                        <td> <?= $Tab[2] ?> </td>
+                                        <td><input type="checkbox" name="checkbox[]" value="<?= $Tab[0] ?>"></td>
+                                    </tr>
+                                <?php
+                            }
+                        ?>
+                    </table>
+                    <input type="submit" name="vendre" value="Vendre">
+                </form>
+            <?php
             // Récupère l'argent du user
             $req = "SELECT user.money FROM `user`, `entite` WHERE user.idPersonnage=entite.id AND entite.id = $idEntite";
             $RequetStatement = $this->_bdd->query($req);
@@ -87,13 +96,10 @@
             }
             $req = "UPDATE `user`, `entite` SET user.money = $money WHERE user.idPersonnage=entite.id AND entite.id = $idEntite";
             $RequetStatement = $this->_bdd->query($req);
-
         }
-
         public function getNomMarche(){
-            return 'Je suis le marché '.$this->_nom;
+            return '<p>Je suis le marché '.$this->_nom.'.</p>';
         }
-
         public function setMarcheById($id){
             parent::setMapById($id);
         }
