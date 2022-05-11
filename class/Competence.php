1f7a1dc33e241beaa5b7ec0b258089e79e89c44e
MrRapidecho <jlanglace@la-providence.net>
2022-05-12 00:41:58 +0200

    add class

---
+++ class/Competence.php
@@ -0,0 +1,13 @@
+﻿<?php
+    //cette classe est développé par : ???
+
+class Competence  {
+
+    private  $id_;
+    private $nom_;
+    private $idCategorie_;
+    private $Information_;	
+    private $idTyp_;
+    
+}
+?>
---
+++ class/Competenceold.php
@@ -0,0 +1,15 @@
+﻿<?php
+    //dev by rapidecho
+    //Une entité à une infinité de compétence
+    class Competence extends CRUD{
+
+        private  $id_;
+        private $nom_;
+        private $idCategorie_;
+        private $Information_;	
+        private $idTyp_;
+
+    }
+
+
+?>
